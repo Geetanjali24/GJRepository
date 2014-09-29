9d0e632e4860c2ee0eeab27a2e6054af8c64ef79
admin <pkumawat@chambal.com>
2014-09-29 03:23:39 -0700

    testing git

---
+++ summation.java
@@ -0,0 +1,14 @@
+
+public class summation {
+		public static void main(String[] args)
+		{
+		int input = 1400;
+		int finalResult = summation(input);
+		}
+	public static int summation(int n)
+	{
+	int result = (n * (n+1))/2;
+	return result;
+	}
+	}
+
